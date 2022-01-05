e271440dcb7d6bc05985b46f1dc209f22b0fae56
Tommy van der Vorst <tommy@pixelspark.nl>
2022-01-05 21:01:19 +0100

    tests: add mobilenet (fallback) test

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ cargo build --release
 ./target/release/nnx infer ./data/mnist.onnx ./data/mnist-7.png --compare
 ./target/release/nnx infer ./data/mnist.onnx ./data/mnist-5.jpg --compare
 ./target/release/nnx infer ./data/squeezenet.onnx ./data/coffee.jpg --compare
-./target/release/nnx infer ./data/mobilenetv2-7.onnx ./data/coffee.jpg --compare
+./target/release/nnx infer ./data/mobilenetv2-7.onnx ./data/coffee.jpg --fallback --labels ./data/mobilenet-labels.txt --top=1
